341138f5c6640df68c01740580c14410534ac569
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WindowsApplication12/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿Namespace WindowsApplication12
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim sideBySideBarSeriesLabel1 As New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Dim sideBySideBarSeriesView1 As New DevExpress.XtraCharts.SideBySideBarSeriesView()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(sideBySideBarSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl1
+            ' 
+            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl1.Name = "chartControl1"
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
+            sideBySideBarSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel1
+            sideBySideBarSeriesView1.AxisXName = "Primary AxisX"
+            sideBySideBarSeriesView1.AxisYName = "Primary AxisY"
+            sideBySideBarSeriesView1.PaneName = "Default Pane"
+            Me.chartControl1.SeriesTemplate.View = sideBySideBarSeriesView1
+            Me.chartControl1.Size = New System.Drawing.Size(575, 366)
+            Me.chartControl1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(575, 366)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            DirectCast(sideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(sideBySideBarSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private WithEvents chartControl1 As DevExpress.XtraCharts.ChartControl
+    End Class
+End Namespace
+
---
+++ VB/WindowsApplication12/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+Imports System.Linq
+
+Namespace WindowsApplication12
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+
+            chartControl1.SeriesTemplate.ChangeView(DevExpress.XtraCharts.ViewType.Bubble)
+            chartControl1.SeriesTemplate.ArgumentDataMember = "Date"
+            chartControl1.SeriesDataMember = "Name"
+            chartControl1.SeriesTemplate.ValueDataMembers.AddRange(New String() { "Value", "Weight" })
+            chartControl1.DataSource = CreateTable()
+
+        End Sub
+
+        Private Function CreateTable() As DataTable
+            Dim data As New DataTable()
+            data.Columns.Add("Name", GetType(String))
+            data.Columns.Add("Date", GetType(Date))
+            data.Columns.Add("Value", GetType(Double))
+            data.Columns.Add("Weight", GetType(Double))
+
+            data.Rows.Add(New Object() { "Aaa", Date.Today.AddDays(0), 7, 4 })
+            data.Rows.Add(New Object() { "Aaa", Date.Today.AddDays(1), 17, 1 })
+            data.Rows.Add(New Object() { "Aaa", Date.Today.AddDays(2), 11, 5 })
+
+            data.Rows.Add(New Object() { "Bbb", Date.Today.AddDays(0), 5, 6 })
+            data.Rows.Add(New Object() { "Bbb", Date.Today.AddDays(1), 13, 9 })
+            data.Rows.Add(New Object() { "Bbb", Date.Today.AddDays(2), 9, 5 })
+
+            data.Rows.Add(New Object() { "Ccc", Date.Today.AddDays(0), 9, 4 })
+            data.Rows.Add(New Object() { "Ccc", Date.Today.AddDays(1), 8, 6 })
+            data.Rows.Add(New Object() { "Ccc", Date.Today.AddDays(2), 7, 5 })
+
+            Return data
+        End Function
+
+        Private Sub chartControl1_BoundDataChanged(ByVal sender As Object, ByVal e As EventArgs) Handles chartControl1.BoundDataChanged
+            Dim chart As ChartControl = DirectCast(sender, ChartControl)
+            Dim infos = chart.Series.Cast(Of Series)().Where(Function(s) TryCast(s.View, BubbleSeriesView) IsNot Nothing).Select(Function(s) New SeriesInfo() With {.View = TryCast(s.View, BubbleSeriesView), .Min = s.Points.Cast(Of SeriesPoint)().Min(Function(p) p.Values(1)), .Max = s.Points.Cast(Of SeriesPoint)().Max(Function(p) p.Values(1))})
+            If infos.Count() > 1 Then
+                Dim minValue As Double = infos.Min(Function(s) s.Min)
+                Dim maxValue As Double = infos.Max(Function(s) s.Max)
+                Dim valueDelta As Double = maxValue - minValue
+                Dim minSize As Double = infos.Min(Function(s) s.View.MinSize)
+                Dim maxSize As Double = infos.Max(Function(s) s.View.MaxSize)
+                Dim sizeDelta As Double = maxSize - minSize
+                For Each si In infos
+                    si.View.MaxSize = maxSize
+                    si.View.MinSize = minSize
+                    si.View.MaxSize = maxSize - sizeDelta * (maxValue - si.Max) / valueDelta
+                    si.View.MinSize = minSize + sizeDelta * (si.Min - minValue) / valueDelta
+                Next si
+            End If
+
+        End Sub
+
+    End Class
+    Public Class SeriesInfo
+        Public Property View() As BubbleSeriesView
+        Public Property Min() As Double
+        Public Property Max() As Double
+    End Class
+End Namespace
